a5f4eee28f03d5de21d4a02dd2fa9140bda39e07
DiegoS965 <71106298+DiegoS965@users.noreply.github.com>
2022-03-21 14:46:05 -0600

    Update index.php
    agregado apartado para mostrar alertas de sesion

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php include ("includes/header.php")?>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 mt-5">
-                <h1 class="text-center">Task Manager</h1>
-                <hr class="height:1px;color: black;background-color:black;">
-            </div>
+<?php include ("db.php") ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-5">
+            <h1 class="text-center">Task Manager</h1>
+            <hr class="height:1px;color: black;background-color:black;">
         </div>
-        <div class="col-md-12">
-            <a href="create_task.php" class="btn btn-success">Create New Task</a>
-            
+    </div>
+    <div class="col-md-12">
+        <?php if(isset($_SESSION['message'])) {?>
+        <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+            <?= $_SESSION['message']?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <?php session_unset(); }?>
+        <a href="create_task.php" class="btn btn-success">Create New Task</a>
+    </div>
+    <div class="col-md-12">
         <table class="table table-hover">
             <thead>
                 <tr>
@@ -28,7 +37,7 @@
                     <a href="delete.php" class="btn btn-danger">
                         <i class="far fa-trash-alt"></i>
                     </a>
-                    
+
                 </td>
             </tbody>
         </table>
