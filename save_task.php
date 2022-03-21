75a5b158381eb373a368161de4044195513b20f0
DiegoS965 <71106298+DiegoS965@users.noreply.github.com>
2022-03-21 14:45:08 -0600

    Update save_task.php
    Conexión a db agregada y mensajes de sesion

--- save_task.php
+++ save_task.php
@@ -1,7 +1,14 @@
 <?php
-//incluir base de dato
-//$_SESSION['message']='Task Saved succesfully';
-//$_SESSION['message_type']='success';
-
-header("Location: index.php");
+    include("db.php");
+    if(isset($_POST['save_task']))
+    {
+        $title = $_POST['taskName'];
+        $description=$_POST['descriptionTask'];
+        
+        $query = "INSERT INTO task(title,description) VALUES ('$title', '$description')";
+        $result=mysqli_query($conn,$query);
+        $_SESSION["message"]='Task saved succesfully';
+        $_SESSION['message_type']='success';
+        header("Location: index.php");
+    }
 ?>
