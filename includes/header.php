23d3d4d804838295bf5e9b96072f1d3211f7fe93
DiegoS965 <71106298+DiegoS965@users.noreply.github.com>
2022-03-21 14:02:59 -0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,18 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP MYSQL CRUD</title>
-    <!--BOOTSRAP 4 -->
+    <title>Task Manager</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!--FONT AWESOME 5-->
-    <script src="https://kit.fontawesome.com/8d3f7d4790.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" intergrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
 <body>
-<nav class= "navbar navbar-dark bg-dark">
-    <div class="container">
-        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
-    </div>
-<form>
-    sadasdasd
-</form>
-</nav>
+    <nav class="navbar navbar-dark bg-dark">
+        <a href="index.php" class="navbar-brand">Task Manager</a>
+    </nav>
