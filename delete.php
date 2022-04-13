8c306d7f269b0e70e8ce4f34e6910b3836a6354b
DiegoS965 <71106298+DiegoS965@users.noreply.github.com>
2022-04-13 12:19:57 -0600

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include ("db.php");
-    require_once("controller.php");
-    if(isset($_GET['IDTarea']))
-    {
-        $delete=new CrudController(new Connection);
-        $delete->deleteTask();
-    }
-?>
